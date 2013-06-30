9d54d9f942708ab6894fa12d78260aef438a1a6a
abanzas <abanzas@localhost>
2013-06-30 11:51:26 -0300

    empiezo a hilvanar paginas de account

---
+++ src/PillReminder/Views/Account/Login.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace PillReminder.Views.Account
+{
+    public partial class Login : PhoneApplicationPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
